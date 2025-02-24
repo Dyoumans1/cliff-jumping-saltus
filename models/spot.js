6b5cc80c3b0f909a6da0f22149364a9f85479d91
Dyoumans <derrikyoumans416@gmail.com>
2025-02-24 13:49:10 -0500

    Added spot schema

---
+++ models/spot.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const spotSchema = new mongoose.Schema({
+_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    auto: true
+},
+user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+
+},
+name: {
+    type: string,
+    required: true
+},
+state: {
+    type: String,
+    required: true
+},
+description: {
+    type: String,
+    required: true
+},
+height: {
+    type: Number,
+    required: true,
+    min: 0
+},
+difficulty: {
+    type: String,
+    required: true
+},
+safe_for_jumping: {
+    type: Boolean,
+    required: true,
+    default: false
+},
+latitude: {
+    type: Number,
+    required: true
+},
+longitude: {
+    type: Number,
+    required: true
+},
+photo: {
+    type: string
+}
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Spot', spotSchema);
+
+
+{/* <label for="photo">Image URL:</label>
+<input type="url" name="photo" value="<%= spot.photo %>" /> */}
